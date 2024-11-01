9842f4bffbc8293781b84de9713f9d20f9ef1db8
larry6b13 <ydss093b17@gmail.com>
2024-11-01 16:10:20 +0800

    Update Register.js

--- Register.js
+++ Register.js
@@ -1,7 +1,33 @@
 export const Register = () => {
   return (
     <div className= 'container'>
-
+        <div className='wrapper'>
+            <div className='register-card'>
+                 <div className='email'>
+                      <span>Email<span/>
+                      <input type='test' placeholder='Email' />
     </div>
+    <div className='username'>
+         <span>Username</span>
+         <input type='text' placeholder='Username' />
+    </div>
+    <div className='password'>
+         <span>Passowrd</span>
+         <input type='passowrd' placeholder='password' />
+    </div>
+    <div className= 'dob'>
+         <span> type='number' placeholder='Day' />
+         <span> type='number' placeholder='Month' />
+         <span> type='number' placeholder='Year' />
+    </div>
+    <div className='btns'>
+        <button className='register-btn'>Continue</button>
+        <div>
+             <a href="#">Already have an account ?</0>
+        <div>
+    </div>
+   </div>
+  </div>
+</div>
   )
 }
